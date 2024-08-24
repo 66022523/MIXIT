d0ba0a8e3d5d4e0ddad218f140f6c6a315c8be60
Chaiwat Suwannarat <66022523@up.ac.th>
2024-08-24 13:52:05 +0700

    🏷️ Spread tag components and update post component.

--- components/post.jsx
+++ components/post.jsx
@@ -9,65 +9,117 @@ import {
   ShareIcon,
 } from "@heroicons/react/24/outline";
 
-import timeSince from "@/utils/timeSince";
+import { TagPill, TagPillPlaceholder } from "./tag";
 
-export function Tag({ header, iconURL, name, url }) {
+export function PostTall({
+  id,
+  imagesSource,
+  imagesAlternate,
+  title,
+  content,
+}) {
   return (
-    <Link
-      href={url || ""}
-      className={
-        "badge link-hover link badge-primary" + (header ? "" : " badge-outline")
-      }
-    >
-      <span className="mr-1">#</span>
-      {iconURL && (
-        <Image
-          className="mr-1 rounded-full"
-          src={iconURL}
-          alt={name}
-          width={16}
-          height={16}
-        />
+    <div className="card flex-row bg-base-200">
+      {imagesSource && (
+        <figure className="py-5 pl-5">
+          <Image
+            className="rounded-xl"
+            src={imagesSource}
+            alt={imagesAlternate}
+            width={100}
+            height={100}
+          />
+        </figure>
       )}
-      <span>{name}</span>
-    </Link>
+      <div className="card-body flex-row p-5">
+        <div className="flex-1">
+          <h2 className="card-title">{title}</h2>
+          <p>{content}</p>
+        </div>
+        <div className="card-actions items-center">
+          <Link href={`/posts/${id}`} className="btn btn-primary">
+            View
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default function Posts({ data, end }) {
-  const id = data.id;
-  const createdAt = data.created_at;
-  const title = data.title;
-  const content = data.content;
-  const tags = data.tags;
-  const images = data.images;
-  const views = data.views;
-  const comments = data.comments;
-  const likes = data.likes;
-  const shares = data.shares;
-  const writer = data.writer;
-  const writerID = writer.id;
-  const writerAvatarURL = writer.avatar_url;
-  const writerNickname = writer.nickname;
-  const circle = data.circle;
-  const circleID = circle.id;
-  const circleIconURL = circle.icon_url;
-  const circleName = circle.name;
+export function Post({
+  id,
+  createdAt,
+  title,
+  content,
+  tags,
+  images,
+  views,
+  likes,
+  comments,
+  shares,
+  writerID,
+  writerAvatarURL,
+  writerNickname,
+  circleID,
+  circleIconURL,
+  circleName,
+  isEnded,
+}) {
+  const timeSince = (input) => {
+    const date = input instanceof Date ? input : new Date(input);
+    const formatter = new Intl.RelativeTimeFormat("en");
+    const ranges = {
+      years: 3600 * 24 * 365,
+      months: 3600 * 24 * 30,
+      weeks: 3600 * 24 * 7,
+      days: 3600 * 24,
+      hours: 3600,
+      minutes: 60,
+      seconds: 1,
+    };
+    const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+    for (let key in ranges) {
+      if (ranges[key] < Math.abs(secondsElapsed)) {
+        const delta = secondsElapsed / ranges[key];
+        return formatter.format(Math.round(delta), key);
+      }
+    }
+  };
 
   return (
     <>
       <div className="card">
-        <div className="card-body">
+        <div className="card-body space-y-4">
           <div className="flex justify-between">
-            <Link href={`/users/${writerID}`} className="flex items-center gap-2">
-              <div className="avatar">
-                <div className="h-12 w-12 rounded-full">
-                  <Image
-                    src={writer ? writerAvatarURL : "/favicon.ico"}
-                    alt=""
-                    width={48}
-                    height={48}
-                  />
+            <Link
+              href={`/users/${writerID}`}
+              className="flex items-center gap-2"
+            >
+              <div
+                className={
+                  writerAvatarURL
+                    ? writerAvatarURL
+                      ? "avatar"
+                      : "avatar placeholder"
+                    : "avatar"
+                }
+              >
+                <div
+                  className={
+                    "h-12 w-12 rounded-full" +
+                    (writerAvatarURL ? "" : " bg-neutral text-neutral-content")
+                  }
+                >
+                  {writerAvatarURL ? (
+                    <Image
+                      alt={writerNickname}
+                      src={writerAvatarURL}
+                      width={48}
+                      height={48}
+                    />
+                  ) : (
+                    <span>{writerNickname.charAt(0)}</span>
+                  )}
                 </div>
               </div>
               <div>
@@ -101,27 +153,28 @@ export default function Posts({ data, end }) {
               </div>
             </div>
           </div>
-          <div className="py-4">
+          <div className="space-y-4">
             <Link href={`/posts/${id}`}>
               <h3 className="card-title">{title}</h3>
               <p>{content}</p>
             </Link>
-            <br />
             <div className="flex flex-wrap gap-2">
-              <Tag
-                header={true}
+              <TagPill
+                isHeader={true}
                 iconURL={circleIconURL}
                 name={circleName}
                 url={`/circles/${circleID}`}
               />
               {tags?.map((tag, index) => (
-                <Tag name={tag.name} url={`/tags/${tag.id}`} key={index} />
+                <TagPill name={tag.name} url={`/tags/${tag.id}`} key={index} />
               ))}
             </div>
-            <br />
-            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+            <div className="grid grid-cols-1 grid-rows-1 gap-4 lg:grid-cols-2 lg:grid-rows-2">
               {images?.map((image, index) => (
-                <picture key={index}>
+                <picture
+                  className={index >= 1 ? "hidden lg:block" : ""}
+                  key={index}
+                >
                   <img
                     className="rounded-2xl"
                     src={image.source}
@@ -141,39 +194,128 @@ export default function Posts({ data, end }) {
               {Intl.NumberFormat("en-US", {
                 notation: "compact",
                 maximumFractionDigits: 1,
-              }).format(views)}
+              }).format(views?.length ?? 0)}
             </div>
             <div className="flex items-center justify-center gap-2 text-sm">
-              <button className="btn btn-circle btn-error btn-sm bg-opacity-20 text-base-content md:btn-md">
+              <button className="btn btn-circle btn-error btn-sm bg-opacity-20 text-base-content md:btn-md hover:text-error-content">
                 <HeartIcon className="size-5" />
               </button>
               {Intl.NumberFormat("en-US", {
                 notation: "compact",
                 maximumFractionDigits: 1,
-              }).format(likes)}
+              }).format(likes?.length ?? 0)}
             </div>
             <div className="flex items-center justify-center gap-2 text-sm">
-              <button className="btn btn-circle btn-primary btn-sm bg-opacity-20 text-base-content md:btn-md">
+              <button className="btn btn-circle btn-primary btn-sm bg-opacity-20 text-base-content md:btn-md hover:text-primary-content">
                 <ChatBubbleBottomCenterIcon className="size-5" />
               </button>
               {Intl.NumberFormat("en-US", {
                 notation: "compact",
                 maximumFractionDigits: 1,
-              }).format(comments.length)}
+              }).format(comments?.length ?? 0)}
             </div>
             <div className="flex items-center justify-center gap-2 text-sm">
-              <button className="btn btn-circle btn-primary btn-sm bg-opacity-20 text-base-content md:btn-md">
+              <button className="btn btn-circle btn-primary btn-sm bg-opacity-20 text-base-content md:btn-md hover:text-primary-content">
                 <ShareIcon className="size-5" />
               </button>
               {Intl.NumberFormat("en-US", {
                 notation: "compact",
                 maximumFractionDigits: 1,
-              }).format(shares)}
+              }).format(shares?.length ?? 0)}
             </div>
           </div>
         </div>
       </div>
-      {!end && <div className="divider" />}
+      {!isEnded && <div className="divider" />}
+    </>
+  );
+}
+
+export function PostPlaceholder({ skeleton, isEnded }) {
+  return (
+    <>
+      <div className="card">
+        <div className="card-body space-y-4">
+          <div className="flex items-center gap-4">
+            <div
+              className={
+                "size-16 shrink-0 rounded-full" +
+                (skeleton ? " skeleton" : " bg-base-300")
+              }
+            />
+            <div className="flex flex-col gap-2">
+              <div
+                className={
+                  "h-4 w-20" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+              />
+              <div
+                className={
+                  "h-4 w-28" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+              />
+            </div>
+          </div>
+          <div className="space-y-4">
+            <div className="space-y-2">
+              <div
+                className={
+                  "h-4 w-20" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+              />
+              <div
+                className={
+                  "h-4 w-40" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+              />
+              <div
+                className={
+                  "h-4 w-48" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+              />
+              <div
+                className={
+                  "h-4 w-48" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+              />
+            </div>
+            <div className="flex flex-wrap gap-2">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <TagPillPlaceholder key={index} />
+              ))}
+            </div>
+            <div className="grid grid-cols-1 grid-rows-1 gap-4 lg:grid-cols-2 lg:grid-rows-2">
+              {Array.from({ length: 4 }).map((_, index) => (
+                <div
+                  className={
+                    "h-32 w-full" +
+                    (skeleton ? " skeleton" : " rounded-box bg-base-300")
+                  }
+                  key={index}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="grid grid-cols-4 gap-2">
+            {Array.from({ length: 4 }).map((_, index) => (
+              <div
+                className={
+                  "mx-auto size-12 rounded-full" +
+                  (skeleton ? " skeleton" : " rounded-full bg-base-300")
+                }
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      {!isEnded && <div className="divider" />}
     </>
   );
 }
