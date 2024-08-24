b37f8bfbae873ab88bd0cafe3d2f55f9cac5828e
Chaiwat Suwannarat <66022523@up.ac.th>
2024-08-24 14:08:36 +0700

    📄 Function for get posts of user.

---
+++ pages/api/v1/users/[id]/posts.js
@@ -0,0 +1,38 @@
+import createClient from "@/utils/supabase/api";
+
+export default async function handler(req, res) {
+  const supabase = createClient(req, res);
+
+  switch (req.method) {
+    case "GET":
+      try {
+        const { data: { posts }, error } = await supabase
+          .from("users")
+          .select("posts (*)")
+          .eq("id", req.query.id)
+          .single();
+
+        if (!posts)
+          return res.status(404).json({
+            message: `No user found or unable to retrieve data.`,
+            error,
+          });
+        if (error)
+          return res.status(500).json({
+            message: "An error occurred while retrieving data.",
+            error,
+          });
+
+        return res.status(200).json(posts);
+      } catch (error) {
+        return res
+          .status(500)
+          .json({ message: "Internal server error", error });
+      }
+    default:
+      return res
+        .status(405)
+        .appendHeader("Allow", "GET")
+        .end(`Method ${req.method} Not Allowed`);
+  }
+}
