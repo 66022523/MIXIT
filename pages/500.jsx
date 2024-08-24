a2e1b391f1cc74bcc9e930170891a855ec4f681c
Chaiwat Suwannarat <66022523@up.ac.th>
2024-08-24 13:54:02 +0700

    😸 Redesign internal error page.

--- pages/500.jsx
+++ pages/500.jsx
@@ -1,3 +1,23 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
 export default function Custom500() {
-  return <h1 className="text-center">500 - Server-side error occurred</h1>;
+  return (
+    <div className="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 space-y-4 text-center">
+      <Player
+        className="h-60"
+        src="/assets/animations/cat-in-library.json"
+        loop
+        autoplay
+      />
+      <div>
+        <h1 className="font-bold">
+          <u>500</u> - Server-side error occurred
+        </h1>
+        <p>
+          There is a temporary problem with the internal system, we are urgently
+          working to fix it.
+        </p>
+      </div>
+    </div>
+  );
 }
