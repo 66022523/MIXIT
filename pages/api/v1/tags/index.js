2224b955f817cb27bdce0a0a38d19561c825dc0e
Chaiwat Suwannarat <66022523@up.ac.th>
2024-08-24 14:03:39 +0700

    🏷️ Function for get all tags.

--- pages/api/v1/tags/index.js
+++ pages/api/v1/tags/index.js
@@ -10,7 +10,16 @@ export default async function handler(req, res) {
           .from("tags")
           .select("*, posts (id)");
 
-        if (error) throw error;
+        if (!data)
+          return res.status(404).json({
+            message: "There are no tag topics available at this time.",
+            error,
+          });
+        if (error)
+          return res.status(500).json({
+            message: "An error occurred while retrieving data.",
+            error,
+          });
 
         return res.status(200).json(
           data.map((tag) => ({
