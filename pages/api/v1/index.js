353e3e6cf5d204537381f2c32bb3634cb5c9a714
Chaiwat Suwannarat <66022523@up.ac.th>
2024-08-24 13:56:27 +0700

    📡 Add ability to check connection between database and app.

--- pages/api/v1/index.js
+++ pages/api/v1/index.js
@@ -1,3 +1,10 @@
+import createClient from "@/utils/supabase/api";
+
 export default async function handler(req, res) {
-  res.status(200).json({ status: true });
+  try {
+    createClient(req, res);
+    return res.status(200).json({ connected: true });
+  } catch (error) {
+    return res.status(500).json({ connected: false, error });
+  }
 }
