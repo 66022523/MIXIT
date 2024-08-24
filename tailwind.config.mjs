645677de620544fc607028bc20cf86aec772d9a5
Chaiwat Suwannarat <66022523@up.ac.th>
2024-08-24 13:35:09 +0700

    🖌️ Update theme and fonts.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,44 @@
-import daisyui from "daisyui"
+import daisyui from "daisyui";
+import { light, dark } from "daisyui/src/theming/themes";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", "var(--font-kanit)"],
+        serif: ["var(--font-inter)", "var(--font-kanit)"],
+        mono: ["var(--font-inter)", "var(--font-kanit)"],
+      },
+    },
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...light,
+          primary: "#006FEE",
+          secondary: "#7828C8",
+          success: "#17C964",
+          warning: "#F5A524",
+          error: "#F31260",
+        },
+        dark: {
+          ...dark,
+          primary: "#006FEE",
+          secondary: "#7828C8",
+          success: "#17C964",
+          warning: "#F5A524",
+          error: "#F31260",
+        },
+      },
+    ],
+  },
 };
