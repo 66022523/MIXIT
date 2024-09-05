8c13bb1d7823bb98ef703e45680edc72097f654c
Chaiwat Suwannarat <66022523@up.ac.th>
2024-09-05 11:50:54 +0700

    🛠️ Add following and followers page of users.

--- pages/users/[id]/followers.jsx
+++ pages/users/[id]/followers.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import useSWR, { SWRConfig } from "swr";
 import { FunnelIcon } from "@heroicons/react/24/outline";
-import { UsersIcon } from "@heroicons/react/24/solid";
+import { UserGroupIcon } from "@heroicons/react/24/solid";
 
 import { Section, NotFound, Content } from "@/components/content";
 import { User } from "@/components/user";
@@ -11,8 +11,8 @@ import { fetcher } from "@/utils/fetcher";
 function Component() {
   const router = useRouter();
   const { data: user } = useSWR(`/api/v1/users/${router.query.id}`);
-  const { data: following } = useSWR(
-    `/api/v1/users/${router.query.id}/following`,
+  const { data: followers } = useSWR(
+    `/api/v1/users/${router.query.id}/followers`,
   );
 
   return (
@@ -20,9 +20,9 @@ function Component() {
       <div className="flex justify-between">
         <Section
           backLink={`/users/${user.id}`}
-          Icon={UsersIcon}
-          title={`${user.nickname} Following`}
-          description={`Users who ${user.nickname} are following.`}
+          Icon={UserGroupIcon}
+          title={`${user.nickname} Followers`}
+          description={`All users who are following ${user.nickname}.`}
         />
         <div className="items-center space-x-2">
           <div className="badge badge-lg gap-2 border-none pl-0">
@@ -30,7 +30,7 @@ function Component() {
               {Intl.NumberFormat("en-US", {
                 notation: "compact",
                 maximumFractionDigits: 1,
-              }).format(following.length || 0)}
+              }).format(followers.length || 0)}
             </div>
             Users
           </div>
@@ -54,8 +54,8 @@ function Component() {
       </div>
       <div className="card bg-base-100">
         <div className="card-body">
-          {following?.length ? (
-            following?.map((user, index) => (
+          {followers?.length ? (
+            followers?.map((user, index) => (
               <User
                 id={user.id}
                 avatarURL={user.avatar_url}
@@ -63,7 +63,7 @@ function Component() {
                 role={user.role}
                 country={user.country}
                 signature={user.signature}
-                isEnded={index + 1 === following.length}
+                isEnded={index + 1 === followers.length}
                 key={index}
               />
             ))
@@ -72,7 +72,7 @@ function Component() {
               <div className="hero-content text-center">
                 <NotFound
                 iconCenter
-                  description={`${user.nickname} hasn't followed anyone at the moment.`}
+                  description={`There are no users following ${user.nickname} yet.`}
                 />
               </div>
             </div>
@@ -83,7 +83,7 @@ function Component() {
   );
 }
 
-export default function Following({ fallback }) {
+export default function Followers({ fallback }) {
   return (
     <SWRConfig value={{ fallback }}>
       <Component />
@@ -95,8 +95,8 @@ export async function getServerSideProps(context) {
   const user = await fetcher(
     `http://localhost:3000/api/v1/users/${context.query.id}`,
   );
-  const following = await fetcher(
-    `http://localhost:3000/api/v1/users/${context.query.id}/following`,
+  const followers = await fetcher(
+    `http://localhost:3000/api/v1/users/${context.query.id}/followers`,
   );
   const circles = await fetcher("http://localhost:3000/api/v1/circles");
   const tags = await fetcher("http://localhost:3000/api/v1/tags");
@@ -104,7 +104,7 @@ export async function getServerSideProps(context) {
     props: {
       fallback: {
         [`/api/v1/users/${context.query.id}`]: user,
-        [`/api/v1/users/${context.query.id}/following`]: following,
+        [`/api/v1/users/${context.query.id}/followers`]: followers,
         "/api/v1/circles": circles,
         "/api/v1/tags": tags,
       },
