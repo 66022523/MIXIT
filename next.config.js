dfd5ae00f67f9d945853b5c4d0b30d58607a1c2b
Chaiwat Suwannarat <66022523@up.ac.th>
2024-09-05 11:26:24 +0700

    🌼 Remove React DaisyUI.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["react-daisyui"],
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "webstatic.hoyoverse.com",
-        pathname: "/**",
-      },
       {
         protocol: "https",
         hostname: "ovscodhpteobdvvqsdvr.supabase.co",
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,6 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "node_modules/daisyui/dist/**/*.js",
-    "node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
     extend: {
