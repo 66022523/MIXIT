3268c3d335fb233fe76860b1609a4cf5bdb27117
Kanyawee <66025997@up.ac.th>
2024-09-11 10:44:35 +0700

    ⚒️ Fix bugs in sign up .

--- app/api/auth/callback/route.js
+++ app/api/auth/callback/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 
-export default async function GET(request) {
+export async function GET(request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
