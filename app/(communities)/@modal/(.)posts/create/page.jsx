f65976ede6e6bb6ee795195721d6054da2322dbd
Chaiwat Suwannarat <dermhioasw123@gmail.com>
2024-10-14 01:21:32 +0700

    📁 Create post page.

---
+++ app/(communities)/@modal/(.)posts/create/page.jsx
@@ -0,0 +1,10 @@
+import Create from "@/components/modals/posts/create";
+
+export default function CreatePost() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-center">Create New Post</h2>
+      <Create />
+    </>
+  );
+}
