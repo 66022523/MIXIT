f267811c5eef4e19165bd93e06080db2cfd53371
Chaiwat Suwannarat <66022523@up.ac.th>
2025-02-16 18:42:12 +0700

    ➕ Add other functions.

--- libs/queries/users.js
+++ libs/queries/users.js
@@ -24,6 +24,30 @@ export async function getUserProfile(id) {
   return { data, error: null };
 }
 
+export async function getUserCircles(id) {
+  if (!id) return { data: [], error: "Please provide ID of user." };
+
+  const url = new URL(`/api/users/${id}/circles`, configs.base_url);
+  const response = await fetch(url.toString());
+  const data = await response.json();
+
+  if (!response.ok) return { data: [], error: data };
+
+  return { data, error: null };
+}
+
+export async function getUserComments(id) {
+  if (!id) return { data: [], error: "Please provide ID of user." };
+
+  const url = new URL(`/api/users/${id}/comments`, configs.base_url);
+  const response = await fetch(url.toString());
+  const data = await response.json();
+
+  if (!response.ok) return { data: [], error: data };
+
+  return { data, error: null };
+}
+
 export async function getUserFollowing(id, count) {
   if (!id) return { user: [], error: "Please provide ID of user." };
 
@@ -40,7 +64,7 @@ export async function getUserFollowing(id, count) {
 }
 
 export async function getUserFollowers(id, count) {
-  if (!id) return { user: [], error: "Please provide ID of user." };
+  if (!id) return { data: [], error: "Please provide ID of user." };
 
   const url = new URL(`/api/users/${id}/followers`, configs.base_url);
 
@@ -54,11 +78,67 @@ export async function getUserFollowers(id, count) {
   return { data, error: null };
 }
 
-export async function getUserCircles(id) {
+export async function getUserLikes(id) {
   if (!id) return { data: [], error: "Please provide ID of user." };
 
-  const url = new URL(`/api/users/${id}/circles`, configs.base_url);
+  const url = new URL(`/api/users/${id}/likes`, configs.base_url);
   const response = await fetch(url.toString());
+  const data = await response.json();
+
+  if (!response.ok) return { data: [], error: data };
+
+  return { data, error: null };
+}
+
+export async function getUserPosts(id) {
+  if (!id) return { data: [], error: "Please provide ID of user." };
+
+  const url = new URL(`/api/users/${id}/posts`, configs.base_url);
+  const response = await fetch(url.toString());
+  const data = await response.json();
+
+  if (!response.ok) return { data: [], error: data };
+
+  return { data, error: null };
+}
+
+export async function getUserReports(session, id) {
+  if (!id) return { data: [], error: "Please provide ID of user." };
+
+  const url = new URL(`/api/users/${id}/reports`, configs.base_url);
+  const response = await fetch(url.toString(), {
+    headers: {
+      authorization: `${session.token_type} ${session.access_token}`
+    }
+  });
+  const data = await response.json();
+
+  if (!response.ok) return { data: [], error: data };
+
+  return { data, error: null };
+}
+
+export async function getUserShares(id) {
+  if (!id) return { data: [], error: "Please provide ID of user." };
+
+  const url = new URL(`/api/users/${id}/shares`, configs.base_url);
+  const response = await fetch(url.toString());
+  const data = await response.json();
+
+  if (!response.ok) return { data: [], error: data };
+
+  return { data, error: null };
+}
+
+export async function getUserViews(session, id) {
+  if (!id) return { data: [], error: "Please provide ID of user." };
+
+  const url = new URL(`/api/users/${id}/views`, configs.base_url);
+  const response = await fetch(url.toString(), {
+    headers: {
+      authorization: `${session.token_type} ${session.access_token}`,
+    },
+  });
   const data = await response.json();
 
   if (!response.ok) return { data: [], error: data };
