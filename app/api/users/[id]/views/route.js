c70a45ce9ca36182b777c2158e3c46df56253ae5
Chaiwat Suwannarat <66022523@up.ac.th>
2025-02-16 18:40:47 +0700

    🛡️ Protected user resources.

--- app/api/users/[id]/views/route.js
+++ app/api/users/[id]/views/route.js
@@ -2,11 +2,42 @@ import { createClient } from "@/utils/supabase/server";
 
 export async function GET(request, { params }) {
   const id = (await params).id;
+
   const searchParams = request.nextUrl.searchParams;
   const limit = searchParams.get("limit") || 10;
 
+  const requestHeaders = new Headers(request.headers);
+  const authorization = requestHeaders.get("authorization");
+
+  if (!authorization)
+    return Response.json(
+      { message: "Missing authorization header" },
+      { status: 400 },
+    );
+
+  const token = authorization.split(" ")[1] || "";
+
   try {
     const supabase = await createClient();
+    const {
+      data: { user },
+      error: authError,
+    } = await supabase.auth.getUser(token);
+
+    if (authError || !user)
+      return Response.json(
+        { message: "Unauthorized", error: authError },
+        { status: 401 },
+      );
+    if (user.id !== id)
+      return Response.json(
+        {
+          message: "There are not enough permissions to access it.",
+          error: authError,
+        },
+        { status: 403 },
+      );
+
     const { data, error } = await supabase
       .from("users")
       .select(
@@ -38,6 +69,20 @@ export async function GET(request, { params }) {
         },
         { status: 500 },
       );
+
+    data.views = Object.values(
+      data.views.reduce((acc, view) => {
+        if (view.post) {
+          const postID = view.post.id;
+          if (!acc[postID]) {
+            acc[postID] = { ...view, view_count: 1 };
+          } else {
+            acc[postID].view_count += 1;
+          }
+        }
+        return acc;
+      }, {}),
+    );
 
     return Response.json(data.views, { status: 200 });
   } catch (error) {
